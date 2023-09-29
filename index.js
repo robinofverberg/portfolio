5b42d20b89ff7113a111605071ba0cf0aa4303e5
robinofverberg <robin.ofverberg@gmail.com>
2023-09-29 13:24:02 +0200

    Commit b4 navbar experiment

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
- const scrollThreshold = 950; // Adjust this value as needed
+let scrollThreshold
 
-        // Function to handle the scroll event
-        function handleScroll() {
-            // Select all elements with the class "change-me"
-            const elementsToChange = document.querySelectorAll(".nav-link");
-            const elementsToAlsoChange = document.querySelectorAll(".navbarChanged");
+if (window.innerWidth <= 480) { // For phone screens
+    scrollThreshold = 450;
+    console.log("You are on a phone resolution")
+} else if (window.innerWidth <= 1920) { // For screens up to 1920x1080
+    scrollThreshold = 890;
+    console.log("You are on a 1920 resolution")
+} else { // For 4k screens and larger
+    scrollThreshold = 1250;
+    console.log("You are on a large resolution")
+}
 
-            // Loop through each element and apply CSS changes
-            elementsToChange.forEach((element) => {
-                // Check if the user has scrolled past the specified point
-                if (window.scrollY > scrollThreshold) {
-                    // Change CSS styles when scrolled past the point
-                    element.style.color = "black";
-                    element.style.fontWeight = "600";
-                } else {
-                    // Reset CSS styles when not scrolled past the point
-                    element.style.color = "rgb(224, 211, 175)";
-                }
-            });
+// Function to handle the scroll event
+function handleScroll() {
+    // Select all elements with the class "change-me"
+    const elementsToChange = document.querySelectorAll(".colorChange");
+    const elementToAlsoChange = document.getElementById("navbar");
 
-            if (window.scrollY > scrollThreshold) {
-                // Change CSS styles when scrolled past the point
-                elementToAlsoChange.style.backgroundColor = "red !important";
-            } else {
-                // Reset CSS styles when not scrolled past the point
-                elementToAlsoChange.style.backgroundColor = "rgba(0, 0, 0, 0.142)";
-            }
+    // Loop through each element and apply CSS changes
+    elementsToChange.forEach((element) => {
+        // Check if the user has scrolled past the specified point
+        if (window.scrollY > scrollThreshold) {
+            // Change CSS styles when scrolled past the point
+            element.style.color = "rgb(173, 168, 162)";
+            element.style.fontWeight = "600";
+        } else {
+            // Reset CSS styles when not scrolled past the point
+            element.style.color = "rgb(224, 211, 175)";
+            element.style.fontWeight = "400";
         }
+    });
 
-        
+    if (window.scrollY > scrollThreshold) {
+        // Change CSS styles when scrolled past the point
+        elementToAlsoChange.style.backgroundColor = "rgba(0, 0, 0, 0.850)";
+    } else {
+        // Reset CSS styles when not scrolled past the point
+        elementToAlsoChange.style.backgroundColor = "rgba(0, 0, 0, 0.142)";
+    }
+}
 
 
-        // Attach the scroll event listener to the window
-        window.addEventListener("scroll", handleScroll);
+
+// DOES NOT WORK OTHERWISE SORRY FOR BAD PRACTISE //:
+window.addEventListener("scroll", handleScroll);
+
+function test() {
+    const elementsToChange = document.querySelectorAll(".colorChange");
+    elementsToChange.forEach((element) => {
+        // Check if the user has scrolled past the specified point
+        element.style.color = "rgb(224, 211, 175)"
+    });
+}
